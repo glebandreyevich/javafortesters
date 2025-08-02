859621fd84b101c689df597d0a981e7615ded505
glebandreyevich <faded5103@gmail.com>
2025-08-02 14:25:12 +0300

    Исключения

--- sandbox/src/main/java/hello.java
+++ sandbox/src/main/java/hello.java
@@ -2,10 +2,24 @@ import java.io.File;
 
 public class hello {
     public static void main(String[] args) {
+        try {
+            var z = calculate();
+            System.out.println(z);
+            System.out.println("Hello World");
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        }
+    }
 
-        System.out.println("Hello World");
-        var configFile=new File("sandbox/build.gradle");
-        System.out.println(configFile.getAbsoluteFile());
-        System.out.println(configFile.exists());
+    private static int calculate() {
+        var x=1;
+        var y=1;
+        var z = divide(x, y);
+        return z;
+    }
+
+    private static int divide(int x, int y) {
+        var z= x / y;
+        return z;
     }
 }
