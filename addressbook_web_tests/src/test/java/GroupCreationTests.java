6ce7b865317de4c36ed13427ee9bdb1e8b2f38b9
glebandreyevich <faded5103@gmail.com>
2025-08-11 15:56:31 +0300

    Задание №5

---
+++ addressbook_web_tests/src/test/java/GroupCreationTests.java
@@ -0,0 +1,67 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GroupCreationTests {
+    private WebDriver driver;
+
+
+    @BeforeEach
+    public void setUp() {
+        if (driver == null) {
+            driver = new FirefoxDriver();
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+            driver.get("https://localhost/addressbook/addressbook/");
+            driver.manage().window().setSize(new Dimension(1303, 702));
+            driver.findElement(By.name("user")).sendKeys("admin");
+            driver.findElement(By.name("pass")).click();
+            driver.findElement(By.name("pass")).sendKeys("secret");
+            driver.findElement(By.cssSelector("input:nth-child(7)")).click();
+        }
+    }
+
+
+    @Test
+    public void CanCreateGroup() {
+        if (!isElementPresent(By.name("new"))) {
+            driver.findElement(By.linkText("groups")).click();
+        }
+        driver.findElement(By.name("new")).click();
+        driver.findElement(By.name("group_name")).click();
+        driver.findElement(By.name("group_name")).sendKeys("name");
+        driver.findElement(By.name("group_header")).click();
+        driver.findElement(By.name("group_header")).sendKeys("header");
+        driver.findElement(By.name("group_footer")).click();
+        driver.findElement(By.name("group_footer")).sendKeys("footer");
+        driver.findElement(By.name("submit")).click();
+    }
+
+    private boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException exception) {
+            return false;
+        }
+    }
+
+    @Test
+    public void CantCreateGroupWithEmptyName() {
+        if (!isElementPresent(By.name("new"))) {
+            driver.findElement(By.linkText("groups")).click();
+        }
+        driver.findElement(By.linkText("groups")).click();
+        driver.findElement(By.name("new")).click();
+        driver.findElement(By.name("group_name")).click();
+        driver.findElement(By.name("group_name")).sendKeys("");
+        driver.findElement(By.name("group_header")).click();
+        driver.findElement(By.name("group_header")).sendKeys("");
+        driver.findElement(By.name("group_footer")).click();
+        driver.findElement(By.name("group_footer")).sendKeys("");
+        driver.findElement(By.name("submit")).click();
+    }
+}
