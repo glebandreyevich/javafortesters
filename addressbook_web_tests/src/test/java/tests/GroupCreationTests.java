52054630c4cb4ead38f3fb8587d00abf2271ebd6
glebandreyevich <faded5103@gmail.com>
2025-08-24 00:30:47 +0300

    Параметризованные тесты

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -2,34 +2,51 @@ package tests;
 
 import model.GroupData;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-public class GroupCreationTests extends TestBase{
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroupCreationTests extends TestBase {
 
 
-    @Test
-    public void CanCreateGroup() {
-        int groupCount = app.groups().getCount();
-        app.groups().CreateGroup(new GroupData("name", "header", "footer"));
-        int newGroupCount = app.groups().getCount();
-        Assertions.assertEquals(groupCount +1,newGroupCount);
-    }
-
-    @Test
-    public void CantCreateGroupWithEmptyName() {
-        app.groups().CreateGroup(new GroupData());
-    }
-    @Test void canCreateGroupWithNameOnly(){
-        app.groups().CreateGroup(new GroupData().withName("some name"));
-    }
-    @Test
-    public void CanCreateGroups() {
-        int n=5;
-        int groupCount = app.groups().getCount();
-        for (int i=0; i<n; i++) {
-            app.groups().CreateGroup(new GroupData(randomString(i*10), "header", "footer"));
+    public static List<GroupData> groupProvides() {
+        var result = new ArrayList<GroupData>();
+        for (var name : List.of("", "group name")) {
+            for (var header : List.of("", "group header")) {
+                for (var footer : List.of("", "group footer")) {
+                    result.add(new GroupData(name, header, footer));
+                }
+            }
         }
+        for (int i = 0; i < 5; i++) {
+            result.add(new GroupData(randomString(i * 10), randomString(i * 10), randomString(i * 10)));
+        }
+        return result;
+    }
+
+    public static List<GroupData> negativegroupProvides() {
+        var result = new ArrayList<GroupData>(List.of(new GroupData("group name'", "", "")));
+        return result;
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("groupProvides")
+    public void CanCreateGroups(GroupData group) {
+        int groupCount = app.groups().getCount();
+        app.groups().CreateGroup(group);
         int newGroupCount = app.groups().getCount();
-        Assertions.assertEquals(groupCount +n,newGroupCount);
+        Assertions.assertEquals(groupCount + 1, newGroupCount);
+    }
+
+    @ParameterizedTest
+    @MethodSource("negativegroupProvides")
+    public void CantCreateGroups(GroupData group) {
+        int groupCount = app.groups().getCount();
+        app.groups().CreateGroup(group);
+        int newGroupCount = app.groups().getCount();
+        Assertions.assertEquals(groupCount, newGroupCount);
     }
 }
