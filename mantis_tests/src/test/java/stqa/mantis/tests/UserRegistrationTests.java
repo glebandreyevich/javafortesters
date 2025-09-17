33e5693e62f6d998c98d5b2918d491798a68ac22
glebandreevich <faded5103@gmail.com>
2025-09-18 00:54:59 +0300

    8.6

---
+++ mantis_tests/src/test/java/stqa/mantis/tests/UserRegistrationTests.java
@@ -0,0 +1,17 @@
+package stqa.mantis.tests;
+
+import org.junit.jupiter.api.Test;
+
+public class UserRegistrationTests extends TestBase{
+
+    @Test
+    void canRegisterUser(){
+        //var email = String.format("%s@localhost",username);
+        //создать пользователя на почтовом сервере(JamesHelper)
+        //заполняем форму создания и отправляем(браузер)
+        //получаем почту(MailHelper)
+        //извлекаем ссылку из письма
+        //проходим по ссылке и завершаем регистрацию(браузер)
+        //проверяем что пользователь может залогиниться(HttpSessionHelper)
+    }
+}
